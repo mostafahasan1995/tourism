c8d5b7c704ecbbd640188893b18909041c9103f9
ferasmn <feras@larsa.org>
2025-06-01 09:07:17 +0300

    invoices

--- query.js
+++ query.js
@@ -1,40 +1,24 @@
 
 
 
-function computeSum (arr) {
-  // The base case is when there is no elements in the array
-  if(arr.length === 0) return 0;
-
-  // for each element of the array am just splicing the array and adding the last element to the whole sum
-  return computeSum(arr.splice(0, arr.length - 1)) + arr[arr.length - 1];
-}
-
-console.log(computeSum([]));
 
 
+function chunk(arr, size){
+  let newArr = []; // buffer for the chunk array
 
-function chunk(arr, size) {
-  // the final result array
-  let result = [];
-  // a pointer to track array elements
-  let pointer = 0;
-  // array to put on the original array elements
-  let subArray = [];
+  for (let i = 0; i < arr.length / size ; i++)
+  {
+      let buf = []; // subarray buffer
+      for (let j = 0 ; j < size ; j++)
+      {
+          if (!arr[j + (i * size)]) // check if there's an item with the corresponding index
+              continue;
 
-  // while loop to go through all array elements
-  while (pointer !== arr.length) {
-    // checking if the subArray length is less than the required size, if so the element get pushed to the subArray and increase the pointer by 1
-    if (subArray.length < size) {
-      subArray.push(arr[pointer]);
-      pointer++;
-    } else { // reaching the required size, push the subArray to the final result and clear the subArray 
-      result.push(subArray);
-      subArray = [];
-    }
+          buf.push(arr[j + (i * size)]); // push to the subarray
+      }
+      newArr.push(buf); // push the subarray to the chunk array
   }
-
-  return result;
+  return newArr;
 }
 
-
-console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2));
+console.log(chunk([1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10], 3));
