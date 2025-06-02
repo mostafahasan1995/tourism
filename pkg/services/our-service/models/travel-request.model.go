059e90d57dda2b5697435ad6c82194a895a9c895
ferasmn <feras@larsa.org>
2025-06-02 15:36:48 +0300

    agent model

--- pkg/services/our-service/models/travel-request.model.go
+++ pkg/services/our-service/models/travel-request.model.go
@@ -35,20 +35,6 @@ type TravelRequestDto struct {
 	SpecialReq      string             `bson:"specialReq" json:"specialReq"`
 }
 
-// type BasicInfo struct {
-// 	ProgramTitle   string     `bson:"programTitle" json:"programTitle"`
-// 	ServiceType    string     `bson:"serviceType" json:"serviceType"`
-// 	Purpose        string     `bson:"purpose" json:"purpose"`
-// 	DelegationType string     `bson:"delegationType" json:"delegationType"`
-// 	Source         string     `bson:"source" json:"source"`
-// 	Customer       string     `bson:"customer" json:"customer"`
-// 	Company        string     `bson:"company" json:"company"`
-// 	Coordinator    string     `bson:"coordinator" json:"coordinator"`
-// 	StartDate      *time.Time `bson:"startDate" json:"startDate"`
-// 	EndDate        *time.Time `bson:"endDate" json:"endDate"`
-// 	GroupSize      string     `bson:"groupSize" json:"groupSize"`
-// }
-
 func (t *TravelRequestDto) Validate(v *validator.Validate) error {
 	return helpers.GenericValidation(v, t)
 }
