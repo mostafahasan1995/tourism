17cbf43bed09b46d0b0e6e1dd641abb12d4b1d00
Mostafa <mostafa.hasan@larsa.org>
2025-12-16 18:11:04 +0200

    Add-customer-status

--- pkg/services/our-service/enums/travel-request.go
+++ pkg/services/our-service/enums/travel-request.go
@@ -3,9 +3,10 @@ package enums
 type TravelReqStatus string
 
 const (
-	TravelReqStatusPending   TravelReqStatus = "pending"
-	TravelReqStatusWaiting   TravelReqStatus = "waiting"
-	TravelReqStatusApproved  TravelReqStatus = "approved"
-	TravelReqStatusRejected  TravelReqStatus = "rejected"
-	TravelReqStatusCompleted TravelReqStatus = "completed"
+	TravelReqStatusPending                   TravelReqStatus = "pending"
+	TravelReqStatusWaitingForCustomerService TravelReqStatus = "waiting-customer"
+	TravelReqStatusWaiting                   TravelReqStatus = "waiting"
+	TravelReqStatusApproved                  TravelReqStatus = "approved"
+	TravelReqStatusRejected                  TravelReqStatus = "rejected"
+	TravelReqStatusCompleted                 TravelReqStatus = "completed"
 )
